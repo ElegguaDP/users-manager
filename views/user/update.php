65a2fe9c7e3b388390ff6f638836f3a4821c824b
Юрій Борисов < >
2016-10-28 18:30:11 +0300

    fix update view

--- views/user/update.php
+++ views/user/update.php
@@ -54,14 +54,13 @@ $this->params['breadcrumbs'][] = 'Update user '.$title;
         </div>
     </div>
     <div class="panel panel-default">
-        <div class="panel-heading"><h4><i class="glyphicon glyphicon-phone"></i> Phones</h4></div>
-        <div class="panel-body">
+        
              <?php DynamicFormWidget::begin([
                 'widgetContainer' => 'dynamicform_wrapper', // required: only alphanumeric characters plus "_" [A-Za-z0-9_]
                 'widgetBody' => '.container-items', // required: css class selector
                 'widgetItem' => '.item', // required: css class
                 //'limit' => 4, // the maximum times, an element can be cloned (default 999)
-                'min' => 1, // 0 or 1 (default 1)
+                'min' => 0, // 0 or 1 (default 1)
                 'insertButton' => '.add-item', // css class
                 'deleteButton' => '.remove-item', // css class
                 'model' => $phones[0],
@@ -70,33 +69,39 @@ $this->params['breadcrumbs'][] = 'Update user '.$title;
                     'phone'
                 ],
             ]); ?>
+            <div class="panel-heading">
+                <h4 class="pull-left"><i class="glyphicon glyphicon-phone "></i> Phones</h4>
+                <button type="button" class="add-item btn btn-success btn-sm pull-right main-add"><i class="glyphicon glyphicon-plus"></i> Add</button>
+            </div>
+            <div class="clearfix"></div>
+            <div class="panel-body">
+                <div class="container-items"><!-- widgetContainer -->
+                <?php foreach ($phones as $i => $phone){ ?>
+                    <div class="item panel panel-default"><!-- widgetBody -->
 
-            <div class="container-items"><!-- widgetContainer -->
-            <?php foreach ($phones as $i => $phone){ ?>
-                <div class="item panel panel-default"><!-- widgetBody -->
-                    
-                    <div class="panel-body">
-                        <?php
-                            // necessary for update action.
-                            if (! $phone->isNewRecord) {
-                                echo Html::activeHiddenInput($phone, "[{$i}]id");
-                            }
-                        ?>
-                        <div class="row">
-                            <div class="col-sm-6">    
-                                <?= $form->field($phone, "[{$i}]phone")->textInput(['maxlength' => true]) ?>
-                            </div>      
-                            <div class="pull-right">
-                                <button type="button" class="add-item btn btn-success btn-xs"><i class="glyphicon glyphicon-plus"></i></button>
-                                <button type="button" class="remove-item btn btn-danger btn-xs"><i class="glyphicon glyphicon-minus"></i></button>
-                            </div>
-                        </div><!-- .row -->
+                        <div class="panel-body">
+                            <?php
+                                // necessary for update action.
+                                if (! $phone->isNewRecord) {
+                                    echo Html::activeHiddenInput($phone, "[{$i}]id");
+                                }
+                            ?>
+                            <div class="row">
+                                <div class="col-sm-6">    
+                                    <?= $form->field($phone, "[{$i}]phone")->textInput(['maxlength' => true]) ?>
+                                </div>      
+                                <div class="pull-right">
+                                    <button type="button" class="add-item btn btn-success btn-xs"><i class="glyphicon glyphicon-plus"></i></button>
+                                    <button type="button" class="remove-item btn btn-danger btn-xs"><i class="glyphicon glyphicon-minus"></i></button>
+                                </div>
+                            </div><!-- .row -->
+                        </div>
                     </div>
+                <?php } ?>
                 </div>
-            <?php } ?>
             </div>
             <?php DynamicFormWidget::end(); ?>
-        </div>
+        
     </div>
 
     <div class="form-group">
