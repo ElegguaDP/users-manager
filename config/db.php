6078c049cf9d98782ecf80f2f743a442d08a9664
Юрій Борисов < >
2016-11-01 18:34:39 +0200

    fix gitignore

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=users_manager',
-    'username' => 'root',
-    'password' => 'developer',
-    'charset' => 'utf8',
-];
